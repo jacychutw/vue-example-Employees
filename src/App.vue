2b133897775a8f3e082dae3c2c7f39e04bab812a
jacychutw <jacychu.tw@gmail.com>
2021-06-26 20:05:42 +0800

    add files

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <InputForm />
+    <DataTable />
+  </div>
+</template>
+
+<script>
+import InputForm from './components/InputForm.vue'
+import DataTable from './components/DataTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    InputForm,
+    DataTable
+  }
+}
+</script>
+
